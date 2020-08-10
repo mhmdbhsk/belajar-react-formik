105408cf3c624ce3dc791a7ae88762d206445a20
Muhammad Bhaska <muhammadbhaska0@gmail.com>
2020-08-10 23:30:11 +0700

    Membuat parameter formik validate

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { useFormik } from 'formik';
 
+const initialValues = {
+  name: 'Azra',
+  email: '',
+  channel: '',
+};
+
+const onSubmit = (values) => {
+  console.log('Form data ', values);
+};
+
+const validate = (values) => {
+  // values.name values.email values.channel
+  // errors.name errors.email errors.channel
+  // errors.name = 'This field is required'
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+
+  return errors;
+};
+
 function YoutubeForm() {
   const formik = useFormik({
-    initialValues: {
-      name: 'Azra',
-      email: '',
-      channel: '',
-    },
-    onSubmit: (values) => {
-      console.log('Form data ', values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
-
-  // console.log('Form values ', formik.values);
-
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
