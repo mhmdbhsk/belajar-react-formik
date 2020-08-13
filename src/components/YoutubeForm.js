d76aa1000bab3cf259b5725edefd863a14ce1f25
Muhammad Bhaska <muhammadbhaska0@gmail.com>
2020-08-13 09:25:17 +0700

    Fix ux validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -56,7 +56,7 @@ function YoutubeForm() {
             onBlur={formik.handleBlur}
             value={formik.values.name}
           />
-          {formik.errors.name ? (
+          {formik.touched.name && formik.errors.name ? (
             <div className="error">{formik.errors.name}</div>
           ) : null}
         </div>
@@ -71,7 +71,7 @@ function YoutubeForm() {
             onBlur={formik.handleBlur}
             value={formik.values.email}
           />
-          {formik.errors.email ? (
+          {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
           ) : null}
         </div>
@@ -86,7 +86,7 @@ function YoutubeForm() {
             onBlur={formik.handleBlur}
             value={formik.values.channel}
           />
-          {formik.errors.channel ? (
+          {formik.touched.channel && formik.errors.channel ? (
             <div className="error">{formik.errors.channel}</div>
           ) : null}
         </div>
