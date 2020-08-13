6a632df79053a0ed54c85ba70d2952d4c6b7b3f6
Muhammad Bhaska <muhammadbhaska0@gmail.com>
2020-08-13 14:12:22 +0700

    Alternatif validation menggunakan yup

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const initialValues = {
   name: 'Azra',
@@ -34,11 +35,18 @@ const validate = (values) => {
   return errors;
 };
 
+const validationSchema = Yup.object({
+  name: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email format').required('Required'),
+  channel: Yup.string().required('Required'),
+});
+
 function YoutubeForm() {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
+    // validate,
   });
 
   console.log('VIsited fields ', formik.touched);
